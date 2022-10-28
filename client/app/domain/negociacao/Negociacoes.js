80417071fcb7615dde61dc52fdb33d77f6f6dc43
Augusto-Moore <aoliveira@moorebrasil.com.br>
2022-10-27 23:26:59 -0300

    Capítulo 9

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,13 +1,11 @@
 class Negociacoes {
   constructor(armadilha) {
     this._negociacoes = [];
-    this._armadilha = armadilha;
     Object.freeze(this);
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
-    this._armadilha(this);
   }
 
   paraArray() {
@@ -21,7 +19,6 @@ class Negociacoes {
     );
   }
   esvazia() {
-    this._negociacoes.lenght = 0;
     this._armadilha(this);
   }
 }
